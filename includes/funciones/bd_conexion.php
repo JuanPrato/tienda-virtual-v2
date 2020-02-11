ea3fd3dac3b9b07b5ff3f40de25ea03f73708edb
JuanPrato <60931246+JuanPrato@users.noreply.github.com>
2020-02-11 12:23:53 -0300

    Update bd_conexion.php

--- includes/funciones/bd_conexion.php
+++ includes/funciones/bd_conexion.php
@@ -1,5 +1,5 @@
 <?php 
-    $conn = new mysqli('localhost', 'root', '', 'proyecto-tienda-v2');
+    $conn = new mysqli('us-cdbr-iron-east-04.cleardb.net', 'bad5f07c31eefe', '4a615280', 'proyecto-tienda-v2');
 
     if($conn->connect_error){
         echo $error -> $conn->connect_error;

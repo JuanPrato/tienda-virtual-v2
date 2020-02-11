17b8e5e43fb6cbcc1ec2abd20a6af403c87c7a2c
JuanPrato <60931246+JuanPrato@users.noreply.github.com>
2020-02-11 12:29:12 -0300

    Update bd_conexion.php

--- includes/funciones/bd_conexion.php
+++ includes/funciones/bd_conexion.php
@@ -1,7 +1,10 @@
-<?php 
-    $conn = new mysqli('us-cdbr-iron-east-04.cleardb.net', 'bad5f07c31eefe', '4a615280', 'proyecto-tienda-v2');
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-    if($conn->connect_error){
-        echo $error -> $conn->connect_error;
-    }
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
 ?>
